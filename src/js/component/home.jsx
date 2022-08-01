4a8448a0a8b11ef26d00faa4321053e0afed39f1
oscq12 <orianasofia86@gmail.com>
2022-08-01 11:44:44 +0000

    :sparkles: inicio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row">
+				<div className="col">
+					titulo
+				</div>
+			</div>
+			<div className="row">
+				<div className="col">
+					<input 
+					type="text"
+					className="data"
+					placeholder="que quieres hacer?" />
+				</div>
+			</div>
+			<div className="row">
+				<div className="col">
+					<ul>
+						<li>contenido de tareas</li>
+					</ul>
+				</div>
+			</div>
+			<p>cuantas tareas llevas hechas</p>
+			
 		</div>
 	);
 };
